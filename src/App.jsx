330be93b56a64ff438eedc2051fed02c84a28258
Alabi Khalid <alabikhalid.12@gmail.com>
2023-09-10 22:44:44 +0100

    routed all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+
 function App() {
-	return <h1>World Wise</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="product" element={<Product />} />
+				<Route path="login" element={<Login />} />
+				<Route path="app" element={<AppLayout />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;

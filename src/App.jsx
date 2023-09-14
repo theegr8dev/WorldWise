a5a7b0c13cfb5491bfabc4f80516910a1d542995
Alabi Khalid <alabikhalid.12@gmail.com>
2023-09-14 23:02:12 +0100

    dynamic storing state in url with URL parameters

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
@@ -6,8 +8,8 @@ import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import CityList from './components/CityList';
-import { useEffect, useState } from 'react';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:8000';
 function App() {
@@ -45,6 +47,7 @@ function App() {
 						path="cities"
 						element={<CityList cities={cities} isLoading={isLoading} />}
 					/>
+					<Route path="cities/:id" element={<City />} />
 					<Route
 						path="countries"
 						element={<CountryList cities={cities} />}
